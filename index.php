a4e4e6fbe78f31991d7fb9ab583d336a9b317969
Jeff Perkinson <jmperkinson@gmail.com>
2020-03-18 12:59:50 -0400

    add auto refresh.

--- index.php
+++ index.php
@@ -8,6 +8,11 @@ include 'inc/functions.php';
 <html lang="en">
 <head>
   <meta charset="UTF-8">
+  <!-- refresh meta data can tell the page to refresh after a set number of seconds 
+  (https://stackoverflow.com/questions/11497611/php-auto-refreshing-page/11497617)
+  use php here to echo the meta refresh -->
+  <!-- set to refresh after 15 seconds -->
+  <?php echo("<meta http-equiv=\"refresh\" content=\"15\" > "); ?>
   <title>Random Quotes</title>
   <link href='https://fonts.googleapis.com/css?family=Playfair+Display:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
   <link rel="stylesheet" href="css/normalize.css">
