cbe332124ccd75766bd0070ae91f2daa27bac9dd
Jeff Perkinson <jmperkinson@gmail.com>
2020-03-17 21:48:21 -0400

    Add  skeleton to datastore.

---
+++ datastore.php
@@ -0,0 +1,18 @@
+<?php
+
+// Data store --> array of associative array quotes for random generation in the UI
+
+$quotes = array(
+    ["quote" => "", "source" => "", "citation" => "", "year" => , "tags" => [] ],
+    ["quote" => "", "source" => "", "citation" => "", "year" => , "tags" => [] ],
+    ["quote" => "", "source" => "", "citation" => "", "year" => , "tags" => [] ],
+    ["quote" => "", "source" => "", "citation" => "", "year" => , "tags" => [] ],
+    ["quote" => "", "source" => "", "citation" => "", "year" => , "tags" => [] ],
+    ["quote" => "", "source" => "", "citation" => "", "year" => , "tags" => [] ],
+    ["quote" => "", "source" => "", "citation" => "", "year" => , "tags" => [] ],
+    ["quote" => "", "source" => "", "citation" => "", "year" => , "tags" => [] ],
+    ["quote" => "", "source" => "", "citation" => "", "year" => , "tags" => [] ],
+    ["quote" => "", "source" => "", "citation" => "", "year" => , "tags" => [] ],
+);
+
+?>
