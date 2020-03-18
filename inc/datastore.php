a1f789bb04caeece173a95d113da516800e57012
Jeff Perkinson <jmperkinson@gmail.com>
2020-03-18 10:36:29 -0400

    Add complete datastore, starter html and css.

---
+++ inc/datastore.php
@@ -0,0 +1,127 @@
+<?php
+
+// Data store --> array of associative array quotes for random generation in the UI
+
+$quotes = array(
+    array( // index 0
+        "quote" => "Baseball is ninety percent mental. The other half is physical.", 
+        "source" => "Yogi Berra",
+        "citation" => NULL,
+        "year" => NULL, 
+        "tags" => array(
+            "#sports", 
+            "#humor",
+            "#baseball",
+        )
+    ),
+    array( // index 1
+        "quote" => "Baseball was, is, and always will be the best game in the world to me.", 
+        "source" => "Babe Ruth",
+        "citation" => NULL,
+        "year" => NULL, 
+        "tags" => array(
+            "#sports",
+            "#baseball",
+        )
+    ),
+    array( // index 2
+        "quote" => "Playing baseball for a living is like having a license to steal.", 
+        "source" => "Pete Rose",
+        "citation" => NULL,
+        "year" => NULL, 
+        "tags" => array(
+            "#sports", 
+            "#humor",
+            "#baseball",
+        )
+    ),
+    array( // index 3
+        "quote" => "The way to make coaches think you're in shape in the spring is to get a tan.", 
+        "source" => "Whitey Ford",
+        "citation" => NULL,
+        "year" => NULL, 
+        "tags" => array(
+            "#sports", 
+            "#humor",
+            "#baseball",
+        )
+    ),
+    array( // index 4
+        "quote" => "I think I was the best baseball player I ever saw.", 
+        "source" => "Willie Mays",
+        "citation" => NULL,
+        "year" => NULL, 
+        "tags" => array(
+            "#sports", 
+            "#humor",
+            "#baseball",
+        )
+    ),
+    array( // index 5
+        "quote" => "Cancer can take away all my physical abilities.  It cannot touch my mind,
+                    it cannot touch my heart, and it cannot touch my soul.
+                    And those three things are going to carry on forever.", 
+        "source" => "Jimmy Valvano",
+        "citation" => "ESPY Awards",
+        "year" => 1993, 
+        "tags" => array(
+            "#sports", 
+            "#life",
+            "#motivation",
+        )
+    ),
+    array( // index 6
+        "quote" => "Fans, for the past two weeks you have been reading about the bad break I got. 
+                    Yet today I consider myself the luckiest man on the face of this earth.", 
+        "source" => "Lou Gehrig",
+        "citation" => "Yankee Stadium",
+        "year" => 1939, 
+        "tags" => array(
+            "#sports", 
+            "#life",
+            "#motivation",
+            "#reflection",
+            "#baseball",
+        )
+    ),
+    array( // index 7
+        "quote" => "Age is no barrier. It’s a limitation you put on your mind.", 
+        "source" => "Jackie Joyner-Kersee",
+        "citation" => NULL,
+        "year" => NULL, 
+        "tags" => array(
+            "#sports", 
+            "#motivation",
+        )
+    ),
+    array( // index 8
+        "quote" => "I think, for me, it came down to being focused on a goal, 
+                    never losing sight of that goal, and being surrounded by people who believed in me.", 
+        "source" => "Chipper Jones",
+        "citation" => "Baseball Hall of Fame - Cooperstown, NY",
+        "year" => 2018, 
+        "tags" => array(
+            "#sports", 
+            "#baseball",
+            "#reflection",
+            "#motivation",
+            "#halloffame",
+        )
+    ),
+    array( // index 9
+        "quote" => "I never gave a thought to the Hall of Fame as I was going through my career. 
+                    My goal as a baseball player was very simple: All I wanted to do was try to get better for my next start. 
+                    And to think it all ended up here, it's pretty cool.", 
+        "source" => "Greg Maddux",
+        "citation" => "Baseball Hall of Fame - Cooperstown, NY",
+        "year" => 2014, 
+        "tags" => array(
+            "#sports", 
+            "#baseball",
+            "#reflection",
+            "#motivation",
+            "#halloffame",
+        )
+    ),
+);
+?>
---
+++ inc/functions.php
@@ -0,0 +1,19 @@
+<?php
+
+// PHP - Random Quote Generator
+// Create the Multidimensional array of quote elements and name it quotes
+// Each inner array element should be an associative array
+// this array resides in datastore.php
+include 'datastore.php';
+//var_dump($quotes);
+
+
+
+
+// Create the getRandomQuuote function and name it getRandomQuote
+
+
+
+// Create the printQuote funtion and name it printQuote
+
+?>
